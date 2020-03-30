4af6c8ae5775f2a48172d6c79261d19cc1e74d70
Wally Haven <wlhaven@gmail.com>
2020-03-30 09:38:17 -0700

    Created menu and added a new report

--- src/com/gator/Main.java
+++ src/com/gator/Main.java
@@ -3,14 +3,7 @@ package com.gator;
 class Main {
 
     public static void main(String[] args) {
-        var totalRows = 0;
-        var readData = new ReadData();
-        String filename = readData.getFileInfo();
-        var resultsList = readData.getData(filename);
-        var db = new Database();
-        db.connect();
-        totalRows = db.SendData(resultsList);
-        System.out.println("Number of rows inserted = " + totalRows );
-        db.close();
+        MainUI mainUI = new MainUI();
+        mainUI.Menu();
     }
 }
